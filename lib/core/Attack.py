c8136c8914caf4c644cae346e4eaea6916c34a01
Aimy <recon.mikasaackerman@gmail.com>
2024-04-06 13:49:12 -0400

    commit

--- lib/core/Attack.py
+++ lib/core/Attack.py
@@ -366,24 +366,26 @@ class Attack:
                     
                     # subprocess.run(command, shell=True, cwd=tool_dir_path)
                     # result = subprocess.run(command, shell=True, cwd=tool_dir_path, text=True, capture_output=True)
+                    if current_category == "exploit":
+                        command = self.exploit_operations.please_exploit_tool(command, tool_name, check_name)
                     
-                    if current_category != "exploit":
-                        proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=tool_dir_path)
-                        stdout, stderr = proc.communicate()
-                    
-                        if tool_name == 'wget' or tool_name == 'wapiti':
-                            pass
-                        else:
-                            # store stdout and stderr
-                            with open(results_file_path, "w") as file:
-                                decoded_output = stdout.decode("utf-8")
-                                cleaned_output = self.matchstring.strip_ansi_codes(decoded_output)
-                                file.write(cleaned_output)
-                                # file.write(stderr)
-                                
-                        # with open(results_file_path, "r") as file:
-                        #     response = file.read()
-                        #     print(response)
+                    print(command)
+                    proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=tool_dir_path)
+                    stdout, stderr = proc.communicate()
+                
+                    if tool_name == 'wget' or tool_name == 'wapiti':
+                        pass
+                    else:
+                        # store stdout and stderr
+                        with open(results_file_path, "w") as file:
+                            decoded_output = stdout.decode("utf-8")
+                            cleaned_output = self.matchstring.strip_ansi_codes(decoded_output)
+                            file.write(cleaned_output)
+                            # file.write(stderr)
+                            
+                    # with open(results_file_path, "r") as file:
+                    #     response = file.read()
+                    #     print(response)
                                       
                 except subprocess.CalledProcessError as e:
                     logger.error(f"Error executing {tool}: {e}\n")
@@ -411,8 +413,6 @@ class Attack:
                     try:
                         if current_category == "vuln":              
                             self.matchstring.process_vuln(tool_name, check_name, results_file_path, vuln_pattern, response, criticality, remed_ref, response_code)
-                        elif current_category == "exploit":
-                            self.exploit_operations.please_exploit_tool(command, tool_name, check_name, results_file_path)
                         elif current_category == "postexploit":
                             pass
                         else:
