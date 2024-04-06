9c42e317cdadea0ff32c0d8478a92314e0002a1e
Aimy <recon.mikasaackerman@gmail.com>
2024-04-06 15:09:01 -0400

    commit

--- lib/core/Attack.py
+++ lib/core/Attack.py
@@ -373,7 +373,7 @@ class Attack:
                             logger.error(f"Error: {e}")
                             pass
                             
-                    subprocess.run(command, shell=True, cwd=tool_dir_path)
+                    # subprocess.run(command, shell=True, cwd=tool_dir_path)
                     proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=tool_dir_path)
                     stdout, stderr = proc.communicate()
                 
