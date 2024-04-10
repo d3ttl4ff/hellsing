9761b09fb819ba057c884329b15296f9151d6901
Aimy <recon.mikasaackerman@gmail.com>
2024-04-10 09:30:00 -0400

    commit

--- lib/core/Attack.py
+++ lib/core/Attack.py
@@ -361,7 +361,7 @@ class Attack:
                     self.output.print_subtitle(display_check_name, display_check_tool_name, tool_description)
                     
                     # self.spinner.start()
-                    scan_start = time.time()
+                    # scan_start = time.time()
                     
                     # subprocess.run(command, shell=True, cwd=tool_dir_path)
                     # result = subprocess.run(command, shell=True, cwd=tool_dir_path, text=True, capture_output=True)
@@ -374,9 +374,10 @@ class Attack:
                             pass
                         # print("\n")
                     
-                    self.spinner.start()
+                    # self.spinner.start()
+                    scan_start = time.time()
                             
-                    # subprocess.run(command, shell=True, cwd=tool_dir_path)
+                    subprocess.run(command, shell=True, cwd=tool_dir_path)
                     proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=tool_dir_path)
                     stdout, stderr = proc.communicate()
                     
